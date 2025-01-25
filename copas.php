3f2a63bc821a4cc011744ba8421cd839188cd7d7
Abdun Syakuur <abdunsyakuur@gmail.com>
2025-01-25 16:17:40 +0700

    Refactor Copas plugin to add GitHub repository link in copas.php

--- copas.php
+++ copas.php
@@ -7,13 +7,10 @@
  * Author URI: https://abduns.com
  * License: GPL-3.0
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * GitHub Plugin URI: https://github.com/swanagata/copas
  */
 
-/**
- * Enqueue the necessary JavaScript files for the Copas plugin.
- *
- * This function includes the main script.js file for the Copas plugin.
- */
+// Enqueue the main script.js file for the Copas plugin
 function copas_enqueue_scripts() {
     // Add the main script.js file for the Copas plugin
     wp_enqueue_script(
@@ -28,22 +25,7 @@ function copas_enqueue_scripts() {
 // Hook the 'copas_enqueue_scripts' function to the 'wp_enqueue_scripts' action
 add_action('wp_enqueue_scripts', 'copas_enqueue_scripts');
 
-/**
- * Shortcode function to create a copy-to-clipboard button.
- *
- * Generates a button allowing users to copy specified content to their clipboard.
- * Several button attributes can be customized using shortcode parameters.
- *
- * @param array $atts {
- *     Optional. An array of attributes to customize the button.
- *
- *     @type string $content        Text to be copied to the clipboard. Default is empty.
- *     @type string $buttontext     The label displayed on the button. Default is 'Copy'.
- *     @type string $successmessage Message shown after a successful copy. Default is 'Copied!'.
- *     @type string $classtext      CSS class applied to the button. Default is 'button'.
- * }
- * @return string HTML button markup for copying content to the clipboard.
- */
+// Add the 'copas' shortcode
 function add_copas_button($atts) {
     $atts = shortcode_atts(
         array(
